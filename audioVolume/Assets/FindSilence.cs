7953b3309f9a063c9469cc3cdbaf080062775511
tharshi <tsworks@protonmail.ch>
2020-01-13 16:26:41 -0500

    updated FindSilence.cs to keep a rolling count of all silences, and choose the longest and latest one

--- audioVolume/Assets/FindSilence.cs
+++ audioVolume/Assets/FindSilence.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+// class for "Silence" storage
+public class Silence
+{
+    public float startTime { get; set; }
+    public int threshold { get; set; }
+}
 
 public class FindSilence : MonoBehaviour
 {
     public AudioClip sound_file;
     public float bpm;
+    public int numSustainBeats;
+    public float thresholdPercentage;
 
     // Start is called before the first frame update
 
@@ -13,18 +23,21 @@ public class FindSilence : MonoBehaviour
     {
         // hyper parameters (4) //
 
-        // the amount of quarter notes judge silence
-        int sustain_beats = 3;
+        // the amount of quarter notes to judge "silence"
+        // this is a MINIMUM constraint (default to 3)
+        int sustain_beats = numSustainBeats;
 
         // buffer time in sec to measure signal response
+        // (default to 150 ms = 0.150 sec)
         float buffer = 0.150f;
 
-        // fraction of the song to analyze
-        // helps avoid soft starts
-        float start_fraction = 0.5f;
+        // latter fraction of the song to analyze
+        // helps avoid soft starts (default to 0)
+        float start_fraction = 0.0f;
 
-        // % of overall RMS to use as threshold
-        float threshold_pct = 0.01f;
+        // % of overall sound levels to use as threshold
+        // (default to 0.01, which is 1%)
+        float threshold_pct = thresholdPercentage;
 
         // ------------------------------------------- //
 
@@ -37,8 +50,8 @@ public class FindSilence : MonoBehaviour
         int window = (int)(buffer * sample_rate);
 
         // convert sustain number of beats to time and samples
-        float sustainTime =  60f * sustain_beats / bpm;
-        int activity_threshold = (int) Mathf.Ceil(sustainTime * sample_rate);
+        float sustain_time =  60f * sustain_beats / bpm;
+        int activity_threshold = (int) Mathf.Ceil(sustain_time * sample_rate);
 
         // grab source samples from 2nd half of song
         int sample_offset = (int)Mathf.Floor(start_fraction * true_num_samples);
@@ -59,11 +72,12 @@ public class FindSilence : MonoBehaviour
         float mean_power = sum_power / num_samples;
         float power_threshold = threshold_pct * mean_power;
 
-        // reset measures for silence detection
+        // reset measures for "silence" detection
         sum_power = 0;
         mean_power = 0;
 
         // set up counting variables
+        int old_num_quiet_steps = activity_threshold;
         int num_quiet_steps = 0;
 
         // some info
@@ -72,11 +86,12 @@ public class FindSilence : MonoBehaviour
         //Debug.Log("Sample Offset = " + sample_offset);
         //Debug.Log("Number of Channels = " + num_channels);
         //Debug.Log("Window Size, in samples = " + window);
-        Debug.Log("Required sustain in sec = " + sustainTime);
+        Debug.Log("Minimum sustain: " + sustain_time + " sec.");
         //Debug.Log("Activity Threshold in Samples = " + activity_threshold);
         Debug.Log("Start Analysis..........");
 
-        bool found_silence = false;
+        // set up return objects
+        var silences = new List<Silence>();
         int start_sample = 0;
 
         // RMS = sqrt(SSA/num_points), volume = 20log10(RMS)
@@ -112,35 +127,55 @@ public class FindSilence : MonoBehaviour
             if (mean_power < power_threshold)
             {
                 num_quiet_steps += 1;
-                
             }
             else
             {
                 num_quiet_steps = 0;
             }
 
-            // if "silence" has been achieved over the required sustain, print out the times
-            if (num_quiet_steps > activity_threshold)
+            // if "silence" has been achieved over the required sustain, record data
+            if (num_quiet_steps > old_num_quiet_steps)
             {
-                // output the time interval where the silence was detected
-                float endingTime = (i + sample_offset) / sample_rate;
-                float startingTime = endingTime - activity_threshold / sample_rate;
-                found_silence = true;
-                Debug.Log("Threshold Reached for Full Sustain Time. The time interval is " + startingTime + " to " + endingTime + " sec.");
+                // add start time and threshold to "silence" recrd
+                int threshold = old_num_quiet_steps;
+                float startTime = (i + sample_offset - old_num_quiet_steps) / sample_rate;
+                silences.Add(new Silence() { startTime = startTime, threshold = threshold});
 
-                // snap to nearest whole bar number and convert to the appropriate number of samples
-                int starting_bars = Mathf.RoundToInt(startingTime * bpm / (4f * 60f));
-                start_sample = Mathf.RoundToInt( starting_bars * 4f * 60f * sample_rate / bpm);
-                Debug.Log("Start replay at bar " + starting_bars + " (" + start_sample + " samples or " + startingTime + " sec)");
+                // update step threshold
+                old_num_quiet_steps = num_quiet_steps;
 
-                break;
             }
 
         }
 
-        if (!found_silence)
+        if (silences.Count == 0)
         {
             Debug.Log("No section of the song is under the RMS threshold for the required sustain time.");
+        }
+        else
+        {
+
+            // "silences" are already sorted, remove early "silences" from list
+            float max_time = silences[silences.Count - 1].startTime;
+            silences.RemoveAll(s => (max_time - s.startTime) > sustain_time);
+
+            // sort the remaining "silences" by sustain time
+            silences = silences.OrderByDescending(s => s.threshold).ToList();
+
+            // grab longest running "silence" step count
+            int specific_step_threshold = silences[0].threshold;
+
+            // calculate time intervals
+            float start_time = silences[0].startTime;
+            float end_time = start_time + specific_step_threshold / sample_rate;
+
+            // snap times to nearest bar and then convert to nearest sample
+            int start_bar = Mathf.RoundToInt(start_time * bpm / (4f * 60f));
+            start_sample = Mathf.RoundToInt(start_bar * 4f * 60f * sample_rate / bpm);
+
+            // Results
+            Debug.Log("Sound level threshold reached. The time interval is " + start_time + " to " + end_time + " sec.");
+            Debug.Log("Start replay at bar " + start_bar + " (" + start_sample + " samples or " + start_time + " sec)");
         }
 
         Debug.Log("Analysis Finished.");
@@ -152,9 +187,4 @@ public class FindSilence : MonoBehaviour
         int s = getRestartSample(sound_file, bpm);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
